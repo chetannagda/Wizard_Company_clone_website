91c691513871ea78bfab01d82d904147a65c582d
Chetan Nagda <chetan23rd@gmail.com>
2025-09-02 01:37:39 +0530

    first commit

---
+++ _next/static/chunks/9c4e2130-e8a9e3d9e1474beb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[512],{1013:(t,e,n)=>{n.d(e,{G8W:()=>i});var r=n(4436);function i(t){return(0,r.k5)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M416 128 192 384l-96-96"},child:[]}]})(t)}}}]);
